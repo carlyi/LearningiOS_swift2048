a18c66c306d44728cb8668e8568c5dac5ce4e7ca
LearniOs <yiyuanping1234@qq.com>
2015-08-30 19:22:20 +0800

    add main Game View

--- test2048View/MainViewController.swift
+++ test2048View/MainViewController.swift
@@ -10,9 +10,76 @@ import UIKit
 
 class MainViewController:UIViewController
 {
+    //game vector setting
+    var dimension:Int = 4
+    //max level 
+    var maxnumber:Int = 2048
+    
+    //width of the box
+    var width:CGFloat = 50
+    //space between the box
+    var padding:CGFloat = 6
+    
+    //save the number of background picture
+    var backgrounds:Array<UIView>!
+    
+    var score:ScoreView!
+    
+    var bestscore:ScoreView!
+    
+    init()
+    {
+        self.backgrounds = Array<UIView>()
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        //set the MainView to white background
+        self.view.backgroundColor = UIColor.whiteColor()
+        setupGameMap()
+        setupScoreLabels()
+    }
+    
+    func setupScoreLabels()
+    {
+        score = ScoreView(stype: ScoreType.Common)
+        score.frame.origin = CGPointMake(50, 80)
+        score.changeScore(value: 0)
+        self.view.addSubview(score)
         
-        self.view.backgroundColor = UIColor.blackColor()
+        bestscore = ScoreView(stype: ScoreType.Best)
+        bestscore.frame.origin.x = 170
+        bestscore.frame.origin.y = 80
+        bestscore.changeScore(value: 0)
+        self.view.addSubview(bestscore)
+    }
+    
+    func setupGameMap()
+    {
+        var x:CGFloat = 50
+        var y:CGFloat = 150
+        
+        for i in 0..<dimension
+        {
+            print(i)
+            y = 150
+            for j in 0..<dimension
+            {
+                //init the View
+                var background = UIView(frame: CGRectMake(x, y, width, width))
+                background.backgroundColor = UIColor.darkGrayColor()
+                
+                self.view.addSubview(background)
+                //save the View for later use
+                backgrounds.append(background)
+                y += padding + width
+            }
+            x += padding + width
+        }
     }
 }
---
+++ test2048View/ScoreView.swift
@@ -0,0 +1,55 @@
+//
+//  ScoreView.swift
+//  test2048View
+//
+//  Created by Carl Yi on 15/8/30.
+//  Copyright (c) 2015年 Carl Yi. All rights reserved.
+//
+
+import UIKit
+enum ScoreType{
+    case Common
+    case Best
+}
+
+protocol ScoreViewProtocol{
+    func changeScore(value s : Int)
+}
+
+class ScoreView: UIView, ScoreViewProtocol
+{
+    var label:UILabel!
+    
+    var defaultFrame = CGRectMake(0, 0, 100, 30)
+    var stype:String!
+    var score: Int = 0 {
+        didSet{
+            //the label is changed as the scores change
+            label.text = "\(stype):\(score)"
+        }
+    }
+    
+    init(stype: ScoreType)
+    {
+        label = UILabel(frame: defaultFrame)
+        label.textAlignment = NSTextAlignment.Center
+        
+        super.init(frame: defaultFrame)
+        
+        self.stype = (stype == ScoreType.Common ? "分数：":"最高分")
+        
+        backgroundColor = UIColor.orangeColor()
+        label.font = UIFont(name: "微软雅黑", size: 16)
+        label.textColor = UIColor.whiteColor()
+        self.addSubview(label)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func changeScore(value s: Int) {
+        score = s
+    }
+}
+
